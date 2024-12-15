0f814e7d999840b081a02a5f6e19a21a329e1de4
Noor Elahi <shamsinoorelahi@gmail.com>
2024-12-15 16:02:55 +0500

    react assg1

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import StrictMode from 'react'
+import ReactDom from 'react-dom/client'
+// import App from './App.jsx'
+
+
+const rootDiv = document.getElementById('root');
+
+ReactDom.createRoot(rootDiv).render(
+<div>
+  
+  <h1>Noor Elahi</h1>
+  <h2>Naveed Ahmed</h2>
+
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores, praesentium?</p>
+   
+   <img src="/vite.svg" alt="car image" width={300} />
+
+</div>
+ 
+
+);
